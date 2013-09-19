27b3b3f88472116001034b924fc36c80e7675e4d
Laurie <github@seldo.com>
2013-09-18 17:07:00 -0700

    This doesn't work, but pipework is in there and it can start vagrant from scratch now

--- commands/start.js
+++ commands/start.js
@@ -1,7 +1,9 @@
 // bring up the specified environment according to the instructions in deploy.json
 var cp = require('child_process'),
+    fs = require('fs-extra'),
   patterns = require('../patterns'),
-  _ = require('underscore')
+  _ = require('underscore'),
+  async = require('async')
 
 module.exports = function(deployData,argv) {
 
@@ -11,8 +13,43 @@ module.exports = function(deployData,argv) {
   var environment = argv.shift()
   if (!environment) environment = 'development'
 
+  startVagrant(0,function() {
+
+    var containerIds = {}
+
+    // start all containers
+
+    // need to work out how to start each one
+    var containerStarters = _.map(
+      // what to look at
+      deployData.resources,
+      // what to do with each one
+      function(resource,name) {
+        return function(cb) {
+          patterns.load(resource.pattern,function(pattern) {
+            cp.exec(
+              "vagrant ssh -c 'sudo docker run -d -v \'/vagrant/" + resource.location + resource.image + "'",
+              function(er,stdout,stderr) {
+                if(er) {
+                  console.log("Failed starting container for " + name)
+                  cb(er)
+                }
+              }
+            )
+          })
+        }
+
+      },
+      // when done with them all:
+      function(er) {
+        connectContainers(deployData)
+      }
+    )
+  })
+
+  /*
   _.each(deployData.resources,function(resource,index) {
-    patterns.load(resource['pattern'],function(pattern) {
+
       var start = pattern['start']
       if (environment == 'development' && pattern['devstart']) {
         start = pattern['devstart']
@@ -37,5 +74,71 @@ module.exports = function(deployData,argv) {
       )
     })
   })
+  */
 
 }
+
+var maxTries = 3
+var startVagrant = function(tries,cb) {
+  if (tries > maxTries) {
+    throw new Error("Could not start vagrant")
+  }
+  console.log("Starting vagrant... (this can take a few minutes the first time)")
+  cp.exec(
+    "vagrant up",
+    function(er,stdout,stderr) {
+      if(er) {
+        if (stdout.indexOf("A Vagrant environment is required")) {
+          if (vagrantExists()) {
+            throw new Error("Vagrantfile exists but not accepted by vagrant")
+          } else {
+            initVagrant(tries,cb)
+          }
+        } else {
+          console.log(er)
+          throw new Error("Failed to start vagrant")
+        }
+      } else {
+        cb() // success!
+      }
+    }
+  )
+}
+
+var vagrantFilePath = process.cwd()+'/Vagrantfile'
+
+var vagrantExists = function() {
+  return fs.existsSync(vagrantFilePath)
+}
+
+var initVagrant = function(tries,cb) {
+  fs.copy(__dirname + "/../scripts/Vagrantfile",vagrantFilePath,function(er) {
+    if(er) {
+      console.log(er)
+      throw new Error("Could not copy Vagrant file to current root")
+    } else {
+      tries++
+      startVagrant(tries,cb)
+    }
+  })
+}
+
+// puts pipework into the running vagrant instance
+var installPlumbing = function(cb) {
+  cp.exec(
+    __dirname + "/../scripts/vagrant-scp.sh " + __dirname + "/../scripts/pipework.sh default:/home/vagrant/.",
+    function(er,stdout,stderr) {
+      if (er) {
+        console.log(er);
+        throw new Error("Failed to bootstrap pipework into vagrant")
+      } else {
+        cb()
+      }
+    }
+  )
+}
+
+var connectContainers = function(deployData) {
+
+}
+
